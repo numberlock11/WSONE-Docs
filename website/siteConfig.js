7b271ba75a4a64413ec2a01fee647d42a9d24670
Naveen Pitchandi <numberlock11@gmail.com>
2019-09-18 20:50:01 -0400

    New page and nav for Google

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,6 +39,7 @@ const siteConfig = {
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     {doc: 'doc1', label: 'Docs'},
+    {doc: 'Google/doc5_1', label: 'Google'},
   ],
 
   // If you have users set above, you add it here:
